a6fb43acc1ec483550778759952ea9144de69ada
Michael Edelman <metalman@amazon.com>
2021-03-23 21:32:32 -0400

    sitemap

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -2,12 +2,20 @@
 
 import { getServerSideSitemap } from "next-sitemap";
 import { GetServerSideProps } from "next";
+import sanity from "../../lib/sanity-client";
+
+const url = (slug: string) => `https://edel.monster/${slug}`;
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // Method to source urls from cms
   // const urls = await fetch('https//example.com/api')
 
-  const fields = [
+  const posts = await sanity.getAll("post");
+  const howtos = await sanity.getAll("howto");
+  const routes = await sanity.getAll("route");
+  const cats = await sanity.getAll("category");
+
+  let fields = [
     {
       loc: "https://edel.monster", // Absolute url
       lastmod: new Date().toISOString(),
@@ -15,6 +23,34 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       // priority
     },
   ];
+
+  for (const post of posts) {
+    fields.push({
+      loc: url(`blog/${post.slug.current}`),
+      lastmod: new Date(post._updatedAt).toISOString(),
+    });
+  }
+
+  for (const howto of howtos) {
+    fields.push({
+      loc: url(`how-to/${howto.slug.current}`),
+      lastmod: new Date(howto._updatedAt).toISOString(),
+    });
+  }
+
+  for (const cat of cats) {
+    fields.push({
+      loc: url(`category/${cat.slug.current}`),
+      lastmod: new Date(cat._updatedAt).toISOString(),
+    });
+  }
+
+  for (const route of routes) {
+    fields.push({
+      loc: url(`${route.slug.current}`),
+      lastmod: new Date(route._updatedAt).toISOString(),
+    });
+  }
 
   return getServerSideSitemap(ctx, fields);
 };
