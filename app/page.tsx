b28223c0a2ee759f14e839ac62f94a59592a3cbb
Michael Edelman <mike.edelman@gmail.com>
2024-01-10 15:52:21 -0500

    wire up next and sanity visual editing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+// ./app/page.tsx
 
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      Coming Soon!
-    </main>
+import { SanityDocument } from "next-sanity";
+import { draftMode } from "next/headers";
+
+import Posts from "@/components/Posts";
+import PostsPreview from "@/components/PostsPreview";
+import { loadQuery } from "@/sanity/lib/store";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+
+export default async function Page() {
+  const initial = await loadQuery<SanityDocument[]>(
+    POSTS_QUERY,
+    {},
+    {
+      perspective: draftMode().isEnabled ? "previewDrafts" : "published",
+    }
+  );
+
+  return draftMode().isEnabled ? (
+    <PostsPreview initial={initial} />
+  ) : (
+    <Posts posts={initial.data} />
   );
 }
