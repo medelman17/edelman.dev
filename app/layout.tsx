4d04ea27ca007152fdd7c2d0c149741cfa8f5f55
Michael Edelman <mike.edelman@gmail.com>
2024-01-12 14:05:32 -0500

    add vercel speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import VisualEditing from "@/components/VisualEditing";
 import { draftMode } from "next/headers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         {draftMode().isEnabled && <VisualEditing />}
+        {<SpeedInsights />}
       </body>
     </html>
   );
