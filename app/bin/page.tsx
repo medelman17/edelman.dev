18a713a20ae4b0acb2a049daea1d99bd157ae001
Michael Edelman <mike.edelman@gmail.com>
2024-01-12 13:40:05 -0500

    move ish around

---
+++ app/bin/page.tsx
@@ -0,0 +1,3 @@
+export default function PasteBinPage() {
+  return <div>Future Home of Paste Bin</div>;
+}
--- components/Posts.tsx
+++ components/Posts.tsx
@@ -3,12 +3,14 @@
 import { SanityDocument } from "next-sanity";
 import Link from "next/link";
 
+import paths from "@/lib/paths";
+
 export default function Posts({ posts }: { posts: SanityDocument[] }) {
   return (
     <main className="container mx-auto grid grid-cols-1 divide-y divide-blue-100">
       {posts?.length > 0 ? (
         posts.map((post) => (
-          <Link key={post._id} href={post.slug.current}>
+          <Link key={post._id} href={paths.blog_post(post.slug.current)}>
             <h2 className="p-4 hover:bg-blue-50">{post.title}</h2>
           </Link>
         ))
